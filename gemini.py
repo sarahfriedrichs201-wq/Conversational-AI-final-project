5c79a44920b2c079d8ef8bad6d1e28ed9ed351e3
jyoung369 <julieyoung360@gmail.com>
2025-11-10 18:27:50 -0500

    Added in system_instructionk to gemini API

--- gemini.py
+++ gemini.py
@@ -1,6 +1,7 @@
 # To install: pip install -q -U google-genai
 
 from google import genai
+from google.genai import types
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
@@ -8,6 +9,10 @@ client = genai.Client()
 context = ""
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=context
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+        system_instruction="You are a large-scale code generation tool. When passed repository context and an instruction, please generate code to match the request."
+    ),
+    contents="Generate me a bubble sort algorithm"
 )
 print(response.text)
